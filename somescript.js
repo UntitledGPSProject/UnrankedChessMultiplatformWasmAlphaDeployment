31e3d06b3165fe0ca9408e5eb131041613698fc3
Ronald Wolvers <ahcwolvers@gmail.com>
2025-03-11 01:41:01 +0100

    Automated re-deployment.

--- somescript.js
+++ somescript.js
@@ -1,9 +1,37 @@
 //import exports from "./module.mjs"
 
-async function logHello() {
-    console.log('Hello');
-    await new Promise(r => setTimeout(r, 2000));
-    console.log('Waited for 2000 ms, calling Kotlin...')
-    const exports = import("./composeApp.mjs");
-    exports.logHelloInKotlin();
+async function makeXMLHttpRequest(method, url, headersMapJsonString) {
+
+    var headers;
+    try {
+        headers = new Map(JSON.parse(headersMapJsonString))
+    } catch (e) {
+        headers = new Map()
+    }
+
+    console.log('Making an arbitrary XMLHttpRequest...')
+    console.log('Method:');
+    console.log(method);
+    console.log('URL:');
+    console.log(url);
+    console.log('Headers:');
+    console.log(headersMapJsonString);
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+           // Typical action to be performed when the document is ready:
+           //document.getElementById("demo").innerHTML = xhttp.responseText;
+           console.log('Received response:');
+           console.log(xhttp.responseText);
+        }
+    };
+    headers.forEach( function () {
+            console.log('Setting request header...');
+            console.log('Request header value: ' + value);
+            console.log('Request header key: ' + key)
+            xhttp.setRequestHeader(value, key)
+        }
+    )
+    xhttp.open(method, url, true);
+    xhttp.send();
 }
